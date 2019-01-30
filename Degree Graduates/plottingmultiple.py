0efdfbd91d32ca58d0c681f8ee84b5b1f4ae4ad2
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-01-30 11:07:50 -0800

    Add files via upload

---
+++ Degree Graduates/plottingmultiple.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.plot(t, temperature, 'r')
+plt. plot(t, dewpoint, 'b')
+plt.xlabel('Date')
+plt.title('Tempterature & Dew Point')
+plt.show() #renders plot object on screen
